37126ca225bac63bb3cba6982d45a26b6f475f0d
MartinConstantini <me.hurtadoconstanti@ugto.mx>
2024-02-22 12:15:13 -0600

    update y delete

--- servidor.js
+++ servidor.js
@@ -6,8 +6,9 @@
 //import {collection,doc,getDoc,getFirestore,setDoc} from 'firebase/firestore'
 import express from 'express';
 import bcrypt from 'bcrypt';
+import cors from 'cors';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc, getDoc,getDocs, setDoc,deleteDoc } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc,getDocs, setDoc,deleteDoc, updateDoc } from 'firebase/firestore';
 import 'dotenv/config';
 
 // Your web app's Firebase configuration
@@ -24,9 +25,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 const db = getFirestore();
+const corsOptions = {
+    origin : '*',
+    optionsSuccessStatus : 200
+}
 const app = express();
 app.use(express.json());
-
+app.use(cors(corsOptions))
 app.get('/', (req, res) => {
     res.send('Respuesta de raiz Martin');
 });
@@ -168,7 +173,7 @@ app.post('/delete-user', async (req, res) => {
         await deleteDoc(userDoc);
 
         res.json({
-            'message': 'Usuario eliminado correctamente'
+            'alerta': 'Usuario eliminado correctamente'
         });
     } catch (error) {
         console.error(error);
@@ -179,6 +184,42 @@ app.post('/delete-user', async (req, res) => {
     }
 });
 
+app.post('/update-user', async (req, res) => {
+    const usuario = req.body;
+    const { password } = req.body;
+
+    try {
+        const usuarios = collection(db, 'usuarios');
+        const userDoc = doc(usuarios, usuario.usuario);
+        const user = await getDoc(userDoc);
+
+        if (!user.exists()) {
+            return res.json({
+                'alerta': 'El usuario no existe en la base de datos'
+            });
+        }
+
+        // Encripta la nueva contraseña si se proporciona
+        if (password && password.length >= 6) {
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+            usuario.password = hash;
+        }
+
+        // Actualiza el documento de usuario con los campos proporcionados
+        await updateDoc(userDoc, usuario);
+
+        res.json({
+            'alerta': 'Usuario actualizado correctamente'
+        });
+    } catch (error) {
+        console.error(error);
+        res.json({
+            'alerta': 'Fallo al actualizar usuario',
+            'message': error
+        });
+    }
+});
 
 const port = process.env.PORT || 6000;
 
